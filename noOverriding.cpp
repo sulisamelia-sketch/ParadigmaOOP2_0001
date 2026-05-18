847ac7b597e6add2957078d2fd78538b007d54a1
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-18 11:05:49 +0700

    Membuat class derivedClass turunan dari baseClass dan menambahkan perkenalan1

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -7,3 +7,10 @@ public:
          cout << "Hallo saya function dari base class";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
